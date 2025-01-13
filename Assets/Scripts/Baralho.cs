e4285fd5adc586185e02ec6d68942756bda48456
Lukas Freitas de Carvalho <lukascarvalho712@gmail.com>
2025-01-13 17:52:51 -0300

    Criação do esqueleto das classes

---
+++ Assets/Scripts/Baralho.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Baralho
+{
+    private int[] cartas;
+
+    public int getCarta(){return 0;}
+    public void insereCarta(int carta){return;}
+    private void embaralhaCartas(){return;}
+}
