9bb4634946c9b9944d2e01a80393cab06bbf5a5f
Vinicius <vinicius.matos@estudante.ufjf.br>
2025-02-02 13:02:46 -0300

    Adiciona função de arrastar a carta

--- Assets/Scripts/Cartas/CartaController.cs
+++ Assets/Scripts/Cartas/CartaController.cs
@@ -5,6 +5,9 @@ public class CartaController : MonoBehaviour
 
 
     private Vector3 pos;
+    private Vector3 offset;
+    private bool segurando = false;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,7 +15,6 @@ public class CartaController : MonoBehaviour
     {
         pos = transform.position;
         Debug.Log("Teste");
-
     }
 
     // Update is called once per frame
@@ -23,10 +25,36 @@ public class CartaController : MonoBehaviour
     }
     void OnMouseEnter()
     {
-        transform.position = pos + new Vector3(0, 0.5f, 0);
+        if (!segurando)
+            transform.position = pos + new Vector3(0, 0.5f, 0);
     }
     void OnMouseExit()
     {
+        if (!segurando)
+            transform.position = pos;
+    }
+
+    void OnMouseUp()
+    {
+        segurando = false;
         transform.position = pos;
     }
+
+    void OnMouseDown()
+    {
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        segurando = true;
+    }
+
+    void OnMouseDrag()
+    {
+        if (segurando)
+        {
+            Vector3 nPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            nPos.z = 0;
+            transform.position = nPos;
+        }
+    }
+
+
 }
